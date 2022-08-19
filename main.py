894ded0c71d2aaa9dfcdcda87b6bd3e92526c1a1
igualmeidam <igualmeidam00@gmail.com>
2022-08-18 21:59:03 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,22 @@
+﻿from fastapi import FastAPI
+from request import list_comprehension
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def home():
+    return 'Funcionando!'
+
+
+@app.get('/heroes}')
+def todos_herois():
+    return list_comprehension
+
+
+@app.get('/heroes/{id_heroes}')
+def get_heroi_por_id(id_heroi: int):
+    for id, heroi in list_comprehension:
+        if id == id_heroi:
+            return heroi
