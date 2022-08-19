11060e7a02331f490edb43bff0bb1b0fa50e1afd
igualmeidam <igualmeidam00@gmail.com>
2022-08-18 22:00:34 -0300

    segundo commit

---
+++ request.py
@@ -0,0 +1,18 @@
+﻿import urllib3
+import json
+
+# Object handling connection pooling and thread safety.
+http = urllib3.PoolManager()
+
+# requests are made with the request() method of the manager instance and
+# providing it with the HTTP method and the desired URL
+lista_herois = http.request(
+    'GET',
+    'https://api.opendota.com/api/heroes'
+)
+
+
+# transformando a informação json em data manipulável para o python
+dados_herois = json.loads(lista_herois.data.decode('utf-8'))
+
+list_comprehension = [[x['id'], x['localized_name']] for x in dados_herois]
